52d0411b11d7a70aa6167a5f81d28aac3b24eea4
evanjoneeswx <evanjnes45@gmail.com>
2022-01-21 16:33:05 -0500

    added in README.txt and hypotenuse functions

--- spatial_calcs.py
+++ spatial_calcs.py
@@ -5,7 +5,7 @@ def haversine(lat1, lon1, lat2, lon2):
         Inputs: lat1, lon1 are the reference coordinates to start with (0 360 for longitude)
                 lat2, lon2 are the coordinates the distance is being calculated to. 
                 Note that lat2/lon2 can be 2D meshgrids as well and would return an array of the distances
-        Outputs: Haversine distance in km (either one value or array of values depending on inputs chosen'''
+        Outputs: Haversine distance in km (either one value or array of values depending on inputs chosen)'''
     # distance between latitudes 
     # and longitudes 
     dLat = (lat2 - lat1) * np.pi / 180.0
@@ -21,3 +21,24 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2 * np.arcsin(np.sqrt(a)) 
     dist = rad * c 
     return dist 
+
+def hypotenuse(res,lat1,lon1):
+    '''This function calculates the hypotenuse distance across a grid box based on the resolution of your dataset (most useful with reanalyses)
+        Inputs: res is the resolution of the dataset in degrees (note that for rectangular resolutions like MERRA2, the function would need to be modified)
+                lat1, lon1 are the reference coordinates to start with (0 360 for longitude)
+                Note that lat1/lon1 can be 2D meshgrids as well and would return an array of the hypotenuses
+        Outputs: Hypotenuse distance in km (either one value or array of values depending on inputs chosen)'''
+    latgrid = lat1-(res/2)
+    longrid = lon1-(res/2)
+    dLat = (latgrid - lat1) * np.pi / 180.0
+    dLon = (longrid - lon1) * np.pi / 180.0
+    lat1 = (lat1) * np.pi / 180.0
+    latgrid = (latgrid) * np.pi / 180.0
+    a = (pow(np.sin(dLat / 2), 2) + 
+            pow(np.sin(dLon / 2), 2) * 
+                np.cos(lat1) * np.cos(latgrid))
+    rad = 6371
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    hypotenuse = rad * c
+    
+    return hypotenuse
