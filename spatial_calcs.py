8e46ea67e40187d53d9bcf0c9cf00a669044e4eb
evanjoneeswx <evanjnes45@gmail.com>
2022-01-21 16:12:28 -0500

    added spatial_calcs.py and deleted testfile.py

--- spatial_calcs.py
+++ spatial_calcs.py
@@ -1,7 +1,11 @@
-
 import numpy as np
 
 def haversine(lat1, lon1, lat2, lon2):  
+    '''This function calculates the Haversine distance on Earth's surface (great circle distance) between two points 
+        Inputs: lat1, lon1 are the reference coordinates to start with (0 360 for longitude)
+                lat2, lon2 are the coordinates the distance is being calculated to. 
+                Note that lat2/lon2 can be 2D meshgrids as well and would return an array of the distances
+        Outputs: Haversine distance in km (either one value or array of values depending on inputs chosen'''
     # distance between latitudes 
     # and longitudes 
     dLat = (lat2 - lat1) * np.pi / 180.0
