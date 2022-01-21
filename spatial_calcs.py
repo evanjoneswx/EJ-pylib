fab45c984d5164b33dc69a33f4acbe950ce621e4
evanjoneeswx <evanjnes45@gmail.com>
2022-01-21 16:52:04 -0500

    added in bearing to spatial calcs

--- spatial_calcs.py
+++ spatial_calcs.py
@@ -40,5 +40,27 @@ def hypotenuse(res,lat1,lon1):
     rad = 6371
     c = 2 * np.arcsin(np.sqrt(a)) 
     hypotenuse = rad * c
-    
     return hypotenuse
+
+def bearing(lat1,lon1,lat2,lon2):
+    '''This function calculates the bearing (from true north CW) on a gridded dataset between two points
+        Inputs: lat1, lon1 are the reference coordinates to start with (0 360 for longitude)
+                lat2, lon2 are the coordinates the bearing is being calculated to. 
+                Note that lat2/lon2 can be 2D meshgrids as well and would return an array of the bearings
+        Outputs: Bearing in degrees (either one value or array of values depending on inputs chosen)'''
+    # constants
+    d2r = np.pi / 180 # convert from degrees to radians
+    r2d = (1/d2r) # convert radians to degrees
+
+    # convert lats/lons from degrees to radians
+    lat1r = lat1*d2r
+    lon1r = lon1*d2r
+    lat2r = lat2*d2r
+    lon2r = lon2*d2r
+
+    # compute angle of motion based on the two lats/lons
+    ang = r2d*np.arctan2(np.sin((lon2r-lon1r))*np.cos(lat2r), np.cos(lat1r)*np.sin(lat2r) - np.sin(lat1r)*np.cos(lat2r)*np.cos(lon2r-lon1r))
+
+    # convert from -180 180 to 0 360
+    ang = ang % 360
+    return ang
